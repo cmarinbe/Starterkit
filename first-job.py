ae1df5ebaf0b21a885677fcb7523dab105c0a2a4
Carla Marin <cmarin@ecm.ub.edu>
2016-11-29 01:04:41 +0100

    send output to lxplus

--- first-job.py
+++ first-job.py
@@ -7,9 +7,8 @@ j.application = myApp
 j.application.readInputData('data/MC_2012_27163003_Beam4000GeV2012MagDownNu2.5Pythia8_Sim08e_Digi13_Trig0x409f0045_Reco14a_Stripping20NoPrescalingFlagged_ALLSTREAMS.DST.py')
 j.backend = Dirac()
 
-## EOS
-f = MassStorageFile('DVntuple.root')
-f.outputfilenameformat = '/starterkit/{jid}_{fname}'
+## output
+f = LocalFile('DVntuple.root')
 j.outputfiles = [f] 
 
 # submit
